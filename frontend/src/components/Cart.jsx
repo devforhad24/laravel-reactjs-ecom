af5ab18d18fafec1938875379e9cf26a75447f50
Hossain Forhad <developerforhad24@gmail.com>
2025-12-13 09:45:24 +0600

    cart update Frontend

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -1,11 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Layout from "../components/common/Layout";
 import { Link } from "react-router-dom";
 import ProductImg from "../assets/images/mens/six.jpg";
 import { CartContext } from "./context/cart";
 
 const Cart = () => {
-  const { cartData, grandTotal, subTotal, shipping } = useContext(CartContext);
+  const {
+    cartData,
+    grandTotal,
+    subTotal,
+    shipping,
+    updateCartItem,
+    deleteCartItem,
+  } = useContext(CartContext);
+  const [qty, setQty] = useState([]);
+
+  const handleQty = (e, itemId) => {
+    const newQty = e.target.value;
+    setQty((...prev) => ({ [itemId]: newQty }));
+    updateCartItem(itemId, newQty);
+  };
+
   return (
     <Layout>
       <div className="container pb-5">
@@ -26,6 +41,18 @@ const Cart = () => {
             <h2 className="border-bottom pb-3">Cart</h2>
             <table className="table">
               <tbody>
+                {cartData.length == 0 && (
+                  <tr>
+                    <td
+                      colSpan={4}
+                      align="center"
+                      valign="middle"
+                      style={{ height: 200 }}
+                    >
+                      Your Cart is empty!
+                    </td>
+                  </tr>
+                )}
                 {cartData &&
                   cartData.map((item) => {
                     return (
@@ -38,31 +65,38 @@ const Cart = () => {
                           <div className="d-flex align-items-center pt-3">
                             <span>${item.price}</span>
                             <div className="ps-3">
-                              {
-                                item.size && <button className="btn btn-size">{item.size}</button>
-                              }
+                              {item.size && (
+                                <button className="btn btn-size">
+                                  {item.size}
+                                </button>
+                              )}
                             </div>
                           </div>
                         </td>
                         <td valign="middle">
                           <input
+                            min={1}
+                            max={10}
                             type="number"
                             style={{ width: "100px" }}
-                            value={item.qty}
+                            value={qty[item.id] || item.qty}
                             className="form-control"
+                            onChange={(e) => handleQty(e, item.id)}
                           />
                         </td>
                         <td valign="middle">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="20"
-                            height="20"
-                            fill="currentColor"
-                            className="bi bi-trash3"
-                            viewBox="0 0 16 16"
-                          >
-                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
-                          </svg>
+                          <a href="#" onClick={() => deleteCartItem(item.id)}>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="20"
+                              height="20"
+                              fill="currentColor"
+                              className="bi bi-trash3"
+                              viewBox="0 0 16 16"
+                            >
+                              <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                            </svg>
+                          </a>
                         </td>
                       </tr>
                     );
@@ -70,7 +104,10 @@ const Cart = () => {
               </tbody>
             </table>
           </div>
-          <div className="row justify-content-end">
+        </div>
+        {
+          cartData.length > 0 &&
+                    <div className="row justify-content-end">
             <div className="col-md-3">
               <div className="d-flex justify-content-between border-bottom pb-2">
                 <div>Subtotal</div>
@@ -91,7 +128,7 @@ const Cart = () => {
               </div>
             </div>
           </div>
-        </div>
+        }
       </div>
     </Layout>
   );
--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Logo from "../../assets/images/logo.png";
 import { Link } from "react-router-dom";
 import { apiUrl } from "./http";
+import { CartContext } from "../context/cart";
+
 
 const Header = () => {
-    const [categories, setCategories] = useState([]);
+  const { getQty } = useContext(CartContext);
+  const [categories, setCategories] = useState([]);
   const fetchCategories = () => {
     fetch(`${apiUrl}/get-categories`, {
       method: "GET",
@@ -25,9 +28,9 @@ const Header = () => {
       });
   };
 
-  useEffect(()=>{
-    fetchCategories()
-  },[])
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
   return (
     <header className="shadow">
@@ -42,13 +45,17 @@ const Header = () => {
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav className="ms-auto my-2 my-lg-0" navbarScroll>
-                {
-                    categories && categories.map(category => {
-                        return (
-                            <Nav.Link key={`cat-${category.id}`} href={`/shop?category=${category.id}`}>{category.name}</Nav.Link>               
-                        )
-                    })
-                }
+              {categories &&
+                categories.map((category) => {
+                  return (
+                    <Nav.Link
+                      key={`cat-${category.id}`}
+                      href={`/shop?category=${category.id}`}
+                    >
+                      {category.name}
+                    </Nav.Link>
+                  );
+                })}
             </Nav>
             <div className="nav-right d-flex">
               <a href="" className="ms-3">
@@ -63,7 +70,8 @@ const Header = () => {
                   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"></path>
                 </svg>
               </a>
-              <Link to="/cart" className="ms-3">
+              <Link to="/cart" className="ms-3 cart-bucket">
+                <span>{getQty()}</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
